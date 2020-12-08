a119da98540ca6dbf2fce8c1009bc2fcfad183d4
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-12-08 16:24:51 -0600

    working on the character creation

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
 import random
 import json
 
-player_data = {"name": "", "hp": 100, "mp": 100, "sp": 100}
+player_data = {
+    "name": "",
+    "hp": 100,
+    "mp": 100,
+    "sp": 100,
+    "str": 10,
+    "dex": 10,
+    "pow": 10,
+    "wil": 10,
+    "vit": 10,
+    "agi": 10,
+    "chr": 10,
+    "end": 10,
+    "luk": 10,
+}
 
 
 def create_character():
     name = input("Please enter your name: ")
+    player_data["name"] = name
+    print("Please select a race for your player:")
+    print("1. Human")
+    print("--- +5 Dexterity")
+    print("--- +5 Charisma")
+    print("--- +5 Luck")
+    print("2. Orc")
+    print("--- +5 Strength")
+    print("--- +5 Willpower")
+    print("--- +5 Endurance")
+    print("3. Frostmere")
+    print("--- +10 Strength")
+    print("--- +5 Vitality")
 
 
 def load_character():
